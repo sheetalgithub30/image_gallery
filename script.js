4215ae4c5c149351c800f09ce31f12917ee39212
sheetal <sheetalkumarilife@gmail.com>
2024-04-26 12:28:13 +0530

    added loader

--- script.js
+++ script.js
@@ -7,11 +7,26 @@ const resultDiv = document.querySelector("#result");
 const more = document.querySelector('#more');
 const paragraph = document.querySelector("paragraph");
 
+const loader = document.querySelector("#loading");
+
+function displayLoading(){
+  loader.classList.add("display");
+  setTimeout(()=>{
+      loader.classList.remove("display");
+  },5000);
+}
+
+function hideLoading(){
+  loader.classList.remove("display");
+}
+
 async function getImage(text,page_no){
+  displayLoading();
 try{
 const respone = await fetch(`https://api.unsplash.com/search/photos?page=${page_no}&query=${text.value}&client_id=${YOUR_ACCESS_KEY}&`);
 const result = await respone.json();
 console.log(result);
+hideLoading();
 
 if(result.total == 0){
   alert("nothing to show")
